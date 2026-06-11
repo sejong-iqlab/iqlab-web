46929c805d8686db664570efe053af7478a9d5e9
dongsunkim07 <dongsun.kim07@gmail.com>
2026-06-11 13:03:19 +0900

    Update next.config.ts
    링크떄매 수정...

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,8 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: '', 
+  assetPrefix: ''
 };
 
 export default nextConfig;
